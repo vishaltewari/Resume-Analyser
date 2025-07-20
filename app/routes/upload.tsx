3e5016e53cd065289ee1c72db3f685ba284c1ebf
Vishal Tiwari <vishaltewari2005@gmail.com>
2025-07-20 16:46:58 +0530

    Added a Page that can show detailed review of Resume

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -53,7 +53,8 @@ const upload = () => {
         data.feedback=JSON.parse(feedbackText)
         await kv.set(`resume:${uuid}`,JSON.stringify(data))
         setStatusText('Analysis complete,redirecting... ')
-        console.log(data)
+        // console.log(data)
+        navigate(`/resume/${uuid}`)
     }
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault()
